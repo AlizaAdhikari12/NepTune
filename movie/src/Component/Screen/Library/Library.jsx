4181a91c3aa50a17497e24c73ed02d3cd901ddd8
Aliza Adhikari <alizaadhikari198@gmail.com>
2024-07-21 00:02:33 +0545

    added library component

--- movie/src/Component/Screen/Library/Library.jsx
+++ movie/src/Component/Screen/Library/Library.jsx
@@ -1,6 +1,31 @@
-export const Library =()=>{
-    return(<>
-    <h2 >Libary</h2>
+import { useState, useEffect } from "react";
+import apiClient from "../../../../spotify";
+
+export const Library = () => {
+  const [playList, setPlayList] = useState([]);
+  useEffect(() => {
+    apiClient.get("me/playlists").then((response) => {
+      setPlayList(response.data.items);
+    }).catch(error=>{
+        console.error("error while fetching data",error);
+    })
+   
+  }, []);
+
+  return (
+    <>
+      {playList.length === 0
+        ? "sorry not data"
+        : playList.map((item) => {
+            return(
+                <div key={item.id}>
+                {item.name}
+                </div>
+            )          
+          })}
+      <h2>Libary</h2>
     </>
-    )
-}
+  );
+};
+
+        
